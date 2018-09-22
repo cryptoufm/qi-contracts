e5765c118d3e4e36dcddede29f5f26a2383a1c3b
Juanro Alvarado <juanroalvarado@Juanros-MacBook-Pro.local>
2018-09-22 13:34:13 -0600

    a

--- contracts/qiRegistry.sol
+++ contracts/qiRegistry.sol
@@ -5,41 +5,48 @@ import  './Collection.sol';
 
 contract qiRegistry {
 
+
+  modifier onlyCollectionOwner(address collectionAddr) {
+    Collection c = Collection(collectionAddr);
+		require(msg.sender == c.getOwner());
+    _;
+  }
+
+  modifier onlyQiOwner(address collectionAddr, address qiAddress) {
+    Collection c = Collection(collectionAddr);
+    Qi q = Qi(qiAddress);
+		require(msg.sender == c.getOwner());
+    require(c.getOwner() == q.getOwner());
+    _;
+  }
+
   // Mapping between profile and Q'i's it created
   mapping (address => address[]) public issuers;
 
   // Mapping between profile and Q'i's it owns
   mapping (address => address[]) public receivers;
 
-  function validateQiIssuer(address qiAddress, address collectionAddress) private view returns (bool){
-    // Function for accessing Qi contract and verifying owner
-    Qi q = Qi(qiAddress);
-    Collection c = Collection(collectionAddress);
-		require(msg.sender == c.getOwner());
-    require(c.getOwner() == q.getOwner());
-    return true;
-  }
-
-  function validateCollection(address collectionAddress) private view returns (bool){
-    // Function for accessing Collection contract and verifying owner
-    Collection c = Collection(collectionAddress);
-		require(msg.sender == c.getOwner());
-    return true;
-  }
-
   /*
     Create a new Qi and register the owner as a Qi Issuer
   */
-  function createQi(address collectionAddr, string title, string info) public {
-    require(validateCollection(collectionAddr));
+  function createQi(
+    address collectionAddr,
+    string title,
+    string info)
+    onlyCollectionOwner (collectionAddr)
+    public {
+
     Qi qi = new Qi(collectionAddr, title, info);
     // Maps the Qi address to the Qi creator
     issuers[collectionAddr].push(qi);
   }
 
-  function issueQi(address qiAddress, address receiverAddress, address collectionAddr) public {
-
-    require(validateQiIssuer(qiAddress, collectionAddr));
+  function issueQi(
+    address qiAddress,
+    address receiverAddress,
+    address collectionAddr)
+    onlyQiOwner (collectionAddr, qiAddress)
+    public {
     // Maps Qi address to the receiver's Q'i' list
     // Sets the date in which the Q'i' was emmited
     receivers[receiverAddress].push(qiAddress);
@@ -47,11 +54,19 @@ contract qiRegistry {
     q.newIssueDate(receiverAddress);
   }
 
-  function getIssuerQis(address issuerAddress) public view returns (address[]){
+  function getIssuerQis(
+    address issuerAddress)
+    public
+    view
+    returns (address[]) {
     return issuers[issuerAddress];
   }
 
-  function getReceiverQis(address receiverAddress) public view returns (address[]) {
+  function getReceiverQis(
+    address receiverAddress)
+    public
+    view
+    returns (address[]) {
     return receivers[receiverAddress];
   }
 
